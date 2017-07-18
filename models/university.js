4be62500793eef68c8d99e1121fe031dd59e4f80
Ben Ford <ford2bm@gmail.com>
2017-07-17 22:21:59 -0400

    fixed spaces to match jslint for air b&b standards

--- models/university.js
+++ models/university.js
@@ -4,15 +4,15 @@ const uniqueValidator = require('mongoose-unique-validator');
 const Schema = mongoose.Schema;
 
 const UniversitySchema = Schema({
-    name: {type: String, required: true, unique: true},
-    nick_name: {type: String, required: true},
-    website: {type: String, required: true},
-    city: {type: String, required: true},
-    state: {type: String, required: true},
-    team: [{type: Schema.ObjectId, ref: 'Team'}],
-    primary_color: {type: String, required: true},
-    secondary_color: {type: String},
-    trinary_color: {type: String},
+  name: { type: String, required: true, unique: true },
+  nick_name: { type: String, required: true },
+  website: { type: String, required: true },
+  city: { type: String, required: true },
+  state: { type: String, required: true },
+  team: [{ type: Schema.ObjectId, ref: 'Team' }],
+  primary_color: { type: String, required: true },
+  secondary_color: { type: String },
+  trinary_color: { type: String },
 });
 
 // Virtual for this university instance
