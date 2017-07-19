ad0045108b4d75c8ca39c3e0ef28dc9489a77987
Ben Ford <ford2bm@gmail.com>
2017-07-19 19:17:35 -0400

    Added basic stadium model test

--- tests/controllers/university.test.js
+++ tests/controllers/university.test.js
@@ -4,53 +4,53 @@ const expect = chai.expect;
 const University = require('../../models/university');
 
 describe('university', () => {
-   it('should be invalid if name is empty', (done) => {
-       let uni = new University();
-       uni.validate((err)=>{
-           expect(err.errors.name).to.exist;
-           done();
-       });
-   });
-    it('should be invalid if nick_name is empty', (done) => {
-        let uni = new University();
-        uni.validate((err)=>{
-            expect(err.errors.nick_name).to.exist;
-            done();
-        });
+  it('should be invalid if name is empty', (done) => {
+    const uni = new University();
+    uni.validate((err) => {
+      expect(err.errors.name).to.exist;
+      done();
     });
-    it('should be invalid if website is empty', (done) => {
-        let uni = new University();
-        uni.validate((err)=>{
-            expect(err.errors.website).to.exist;
-            done();
-        });
+  });
+  it('should be invalid if nick_name is empty', (done) => {
+    const uni = new University();
+    uni.validate((err) => {
+      expect(err.errors.nick_name).to.exist;
+      done();
     });
-    it('should be invalid if city is empty', (done) => {
-        let uni = new University();
-        uni.validate((err)=>{
-            expect(err.errors.city).to.exist;
-            done();
-        });
+  });
+  it('should be invalid if website is empty', (done) => {
+    const uni = new University();
+    uni.validate((err) => {
+      expect(err.errors.website).to.exist;
+      done();
     });
-    it('should be invalid if state is empty', (done) => {
-        let uni = new University();
-        uni.validate((err)=>{
-            expect(err.errors.state).to.exist;
-            done();
-        });
+  });
+  it('should be invalid if city is empty', (done) => {
+    const uni = new University();
+    uni.validate((err) => {
+      expect(err.errors.city).to.exist;
+      done();
     });
-    it('should be invalid if primary_color is empty', (done) => {
-        let uni = new University();
-        uni.validate((err)=>{
-            expect(err.errors.primary_color).to.exist;
-            done();
-        });
+  });
+  it('should be invalid if state is empty', (done) => {
+    const uni = new University();
+    uni.validate((err) => {
+      expect(err.errors.state).to.exist;
+      done();
     });
-    it('should be invalid if secondary_color is empty', (done) => {
-        let uni = new University();
-        uni.validate((err)=>{
-            expect(err.errors.secondary_color).to.exist;
-            done();
-        });
+  });
+  it('should be invalid if primary_color is empty', (done) => {
+    const uni = new University();
+    uni.validate((err) => {
+      expect(err.errors.primary_color).to.exist;
+      done();
     });
+  });
+  it('should be invalid if secondary_color is empty', (done) => {
+    const uni = new University();
+    uni.validate((err) => {
+      expect(err.errors.secondary_color).to.exist;
+      done();
+    });
+  });
 });
---
+++ tests/models/statdium.test.js
@@ -0,0 +1,9 @@
+const sinon = require('sinon');
+const chai = require('chai');
+
+const expect = chai.expect;
+
+const mongoose = require('mongoose');
+require('sinon-mongoose');
+
+// TODO: Test Stadium Paths
