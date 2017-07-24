a9505d6c3b39b62ff4d860c7e5fb81e04d00d87d
Bunit73 <ford2bm@gmail.com>
2017-07-24 16:49:01 -0400

    conference init

--- models/coach.js
+++ models/coach.js
@@ -1,1 +1,19 @@
-const mongoose = require('mongoose');const uniqueValidator = require('mongoose-unique-validator');const Schema = mongoose.Schema;const CoachSchema = Schema({  first_name: { type: String, required: true },  last_name: { type: String, required: true },  alma_mater: { type: String },  dob: { type: Date },  teams: [{ type: Schema.ObjectId, ref: 'Team' }],}, { collection: 'coaches' });CoachSchema    .virtual('url')    .get(function () { `/coach/${this._id}`; });CoachSchema.plugin(uniqueValidator);module.exports = mongoose.model('Coach', CoachSchema);
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const Schema = mongoose.Schema;
+
+const CoachSchema = Schema({
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
+  alma_mater: { type: String },
+  dob: { type: Date },
+  teams: [{ type: Schema.ObjectId, ref: 'Team' }],
+}, { collection: 'coaches' });
+
+CoachSchema
+    .virtual('url')
+    .get(function () { `/coach/${this._id}`; });
+
+CoachSchema.plugin(uniqueValidator);
+module.exports = mongoose.model('Coach', CoachSchema);
---
+++ models/conference.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const Schema = mongoose.Schema;
+
+const ConferenceSchema = Schema({
+  name: { type: String, required: true, unique: true },
+  abbreviation: { type: String, required: true, unique: true },
+  date_founded: { type: Date },
+  website: { type: String },
+  conf_seasons: [{ type: Schema.ObjectId, ref: 'ConfSeason' }],
+}, { collection: 'conferences' });
+
+// Virtual for the conference instance
+ConferenceSchema
+    .virtual('url')
+    .get(function () { `/conferences/${this._id}`; });
+
+ConferenceSchema.plugin(uniqueValidator);
+module.exports = mongoose.models('Conference', ConferenceSchema);
